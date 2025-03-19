2c7a016775fe6daac38ecbc4d5b76e5de2fd9477
ricky-kiva <ricky.kiva@gmail.com>
2025-03-19 14:29:31 +0700

    feat(exc): add Exercise 12.7

---
+++ todo-app/todo-backend/routes/exercise12_7.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const redis = require('../redis');
+const { Todo } = require('../mongo')
+const router = express.Router();
+
+/* GET todos listing. */
+router.get('/', async (_, res) => {
+  const todos = await Todo.find({})
+  res.send(todos);
+});
+
+/* POST todo to listing. */
+router.post('/', async (req, res) => {
+  const todo = await Todo.create({
+    text: req.body.text,
+    done: false
+  });
+
+  const addedTodos = await redis.getAsync('added_todos');
+  
+  if (addedTodos === null  || isNaN(parseInt(addedTodos))) {
+    await redis.setAsync('added_todos', 0);
+    return res.send(todo);
+  }
+
+  await redis.setAsync('added_todos', parseInt(addedTodos) + 1);
+
+  res.send(todo);
+});
+
+const singleRouter = express.Router();
+
+const findByIdMiddleware = async (req, res, next) => {
+  const { id } = req.params
+  req.todo = await Todo.findById(id)
+  if (!req.todo) return res.sendStatus(404)
+
+  next()
+}
+
+/* DELETE todo. */
+singleRouter.delete('/', async (req, res) => {
+  await req.todo.delete()  
+  res.sendStatus(200);
+});
+
+/* GET todo. */
+singleRouter.get('/', async (req, res) => {
+  res.json(req.todo);
+});
+
+/* PUT todo. */
+singleRouter.put('/', async (req, res) => {
+  const { text, done } = req.body;
+
+  req.todo.text = text ?? req.todo.text;
+  req.todo.done = done ?? req.todo.done;
+
+  await req.todo.save();
+
+  res.json(req.todo);
+});
+
+router.use('/:id', findByIdMiddleware, singleRouter);
+
+module.exports = router;
